a690757c5a8418d11342fd30bdaa450f53bfaf98
gavasquez <gustavovasquez15@gmail.com>
2025-03-23 14:23:23 -0500

    Se remueve la sombra de las tarjetas

--- src/components/ui/services/ServicesGrid.tsx
+++ src/components/ui/services/ServicesGrid.tsx
@@ -14,9 +14,6 @@ export default function ServicesGrid( { service, siteColor }: Props ) {
   return (
     <div
       className="p-6 transition duration-500 rounded-2xl mt-6 hover:shadow-2xl hover:transition-shadow hover:scale-105 hover:shadow-slate-900/50"
-      style={ {
-        boxShadow: `0 15px 30px rgba(${ parseInt( ( siteColor || '#000' ).slice( 1, 3 ), 16 ) }, ${ parseInt( ( siteColor || '#000' ).slice( 3, 5 ), 16 ) }, ${ parseInt( ( siteColor || '#000' ).slice( 5, 7 ), 16 ) }, 0.7)`,
-      } }
     >
       <div className="flex justify-center items-center">
           <div className="w-48 h-48 bg-indigo-600/5 rounded-xl text-3xl flex justify-center items-center shadow-sm shadow-gray-800">
